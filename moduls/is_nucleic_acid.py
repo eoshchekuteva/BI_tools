68ced3fed73f4af8ad39d101ed11219b20b7efa6
Ekaterina <eoshchekuteva@gmail.com>
2025-10-04 20:05:39 +0300

    Readjusted the function check

--- moduls/is_nucleic_acid.py
+++ moduls/is_nucleic_acid.py
@@ -1,10 +1,10 @@
 def is_dna(sequence):
     dna = set("AGTC")
-    return set(sequence) <= dna
+    return set(sequence.upper()) <= dna
 
 def is_rna(sequence):
     rna = set("AUGC")
-    return set(sequence) <= rna
+    return set(sequence.upper()) <= rna
 
 def is_nucleic_acid(sequence):
     return is_dna(sequence) or is_rna(sequence)
