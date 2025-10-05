6a24748c49329f720d86b56e6d863983e6d1e2b8
Ekaterina <eoshchekuteva@gmail.com>
2025-10-05 12:01:30 +0300

    Add reverse function

--- moduls/transcription_funcs.py
+++ moduls/transcription_funcs.py
@@ -5,8 +5,22 @@ def transcribe(sequence: str) -> str:
     Argument:
     sequence (str): The input DNA sequence.
 
-    Returns str: 
+    Returns str:
     The transcribed RNA sequence.
     """
     transcribe_sequence = sequence.replace("T", "U").replace("t", "u")
     return transcribe_sequence
+
+
+def reverse(sequence: str) -> str:
+    """
+    Return the reversed sequence of the given sequence.
+
+    Argument:
+    sequence (str): the input sequence.
+
+    Return str:
+    the reversed sequence.
+    """
+    reverse_sequence = sequence[::-1]
+    return reverse_sequence
